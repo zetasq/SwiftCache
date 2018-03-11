096f49093e423f0ec7a885e4a92965da5905ed65
Zhu Shengqi <zetasq@gmail.com>
2018-03-11 22:51:33 +0800

    Initial Commit

---
+++ SwiftCache/SwiftCache.h
@@ -0,0 +1,19 @@
+//
+//  SwiftCache.h
+//  SwiftCache
+//
+//  Created by Zhu Shengqi on 11/03/2018.
+//  Copyright © 2018 Zhu Shengqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftCache.
+FOUNDATION_EXPORT double SwiftCacheVersionNumber;
+
+//! Project version string for SwiftCache.
+FOUNDATION_EXPORT const unsigned char SwiftCacheVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftCache/PublicHeader.h>
+
+
